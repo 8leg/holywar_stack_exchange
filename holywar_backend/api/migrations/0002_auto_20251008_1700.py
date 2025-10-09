fc4f9519585dc77850ad3ffda8700eacd46951a9
8leg <vovkin122@gmail.com>
2025-10-09 17:24:35 +0300

    minor comment change and also testing some git shit

--- holywar_backend/api/migrations/0002_auto_20251008_1700.py
+++ holywar_backend/api/migrations/0002_auto_20251008_1700.py
@@ -23,8 +23,6 @@ def add_default_users(apps, schema_editor):
         username='Deleted',
         access_level='user',
         email='noreply@gmail.com',
-        # okay so I didn't know how to make an unusable user hash so I asked
-        # LLM and it told me one-char hash won't pass the test, so I guess I gotta trust em cause I'm scared to use another lib
         passwd='!',
     )
 
